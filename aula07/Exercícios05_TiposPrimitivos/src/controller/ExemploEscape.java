a92399848317ec180f6ac8915860be7893e6c967
leodande <44007897+leodande@users.noreply.github.com>
2018-10-18 21:10:46 -0300

    Add files via upload

---
+++ aula07/Exercícios05_TiposPrimitivos/src/controller/ExemploEscape.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class ExemploEscape {
+	public static void main(String[] args) {
+		
+		System.out.println("\t Utilizando TAB");
+		System.out.println("\n\n2x Quebre de linha");
+		System.out.println("\\Contra-Barra");
+		System.out.println("\'Apas Simples\'");
+		System.out.println("\"Aspas Duplas\"");
+	}
+
+}
