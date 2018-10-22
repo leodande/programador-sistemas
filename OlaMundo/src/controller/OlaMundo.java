25865bf42839a640c18074e45cf2af17f686b4ca
leodande <44007897+leodande@users.noreply.github.com>
2018-10-22 20:59:41 -0300

    Add files via upload

---
+++ OlaMundo/src/controller/OlaMundo.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class OlaMundo {
+	/*
+	 * Este é o meu pimeiro programa JAVA
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Ola Mundo!");
+		System.out.println(10);
+	}
+}
