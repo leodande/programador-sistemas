4c4da65552dfe89d234ab92e9c2ad1cc612a43b0
leodande <44007897+leodande@users.noreply.github.com>
2018-10-26 21:44:01 -0300

    Add files via upload

---
+++ aula13/13_Vetor/src/controller/ExemploVetorInt.java
@@ -0,0 +1,23 @@
+package controller;
+
+public class ExemploVetorInt {
+	
+	public static void main(String[] args) {
+		
+		int numeros[] = new int[10];
+		numeros[0] = 6;
+		numeros[2] = 17;
+		
+		System.out.println("numeros[0] = " + numeros[0]);
+		System.out.println("numeros[1] = " + numeros[1]);
+		System.out.println("numeros[2] = " + numeros[2]);
+		System.out.println("numeros[3] = " + numeros[3]);
+		System.out.println("nuemros[4] = " + numeros[4]);
+		System.out.println("numeros[5] = " + numeros[5]);
+		System.out.println("numeros[6] = " + numeros[6]);
+		System.out.println("numeros[7] = " + numeros[7]);
+		System.out.println("numeros[8] = " + numeros[8]);
+		System.out.println("nuemros[9] = " + numeros[9]);
+	}
+
+}
