e01a7bdaf786e5b0b8609907c5b26d9c611b17f1
leodande <44007897+leodande@users.noreply.github.com>
2018-10-26 21:40:59 -0300

    Add files via upload

---
+++ aula12/12_AlgoritimoJava/src/controller/LacoTesteInicio.java
@@ -0,0 +1,18 @@
+package controller;
+
+import util.Teclado;
+
+public class LacoTesteInicio {
+
+	public static void main(String[] args) {
+		int r, x, i;
+		i = 1;
+		x = Teclado.lerInt("Digite um número: ");
+		while (i <= 5) {
+			r = x * 3;
+			System.out.println(r);
+			// i = i + i;
+			i++;
+		}
+	}
+}
