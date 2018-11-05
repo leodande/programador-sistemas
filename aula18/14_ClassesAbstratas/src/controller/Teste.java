8ee64af21606f8f464c8589669d5aaf347ced0a2
leodande <44007897+leodande@users.noreply.github.com>
2018-11-05 21:52:56 -0200

    Add files via upload

---
+++ aula18/14_ClassesAbstratas/src/controller/Teste.java
@@ -0,0 +1,14 @@
+package controller;
+import model.*;
+
+public class Teste {
+	
+	public static void main(String[] args) {
+		
+		//Usuario usu = new Usuario();
+		Professor prof = new Professor();
+		Aluno aluno = new Aluno();
+		
+	}
+
+}
---
+++ aula18/14_ClassesAbstratas/src/model/Aluno.java
@@ -0,0 +1,78 @@
+package model;
+
+public class Aluno extends Usuario {
+	
+	private int matriculaAluno;
+	private String nome;
+	private String turno;
+	private String turma;
+	private int tipo;
+	
+	
+	public int getMatriculaAluno() {
+		return matriculaAluno;
+	}
+
+	public void setMatriculaAluno(int matriculaAluno) {
+		this.matriculaAluno = matriculaAluno;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTurno() {
+		return turno;
+	}
+
+	public void setTurno(String turno) {
+		this.turno = turno;
+	}
+
+	public String getTurma() {
+		return turma;
+	}
+
+	public void setTurma(String turma) {
+		this.turma = turma;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public Aluno() {
+		super();
+	}
+	
+	public Aluno(String nome, String turno, String turma, int matriculaAluno ) {
+		super();
+		
+		this.matriculaAluno = matriculaAluno;
+		this.nome = nome;
+		this.turno = turno;
+		this.turma = turma;
+		
+	}
+	
+	@Override
+	public void mostrar() {
+		System.out.println("Informações do aluno: ");
+		System.out.println("Login : " + super.getLogin());
+		System.out.println("Senha: *****");
+		
+		System.out.println("Matrícula: " + matriculaAluno);
+		System.out.println("Nome : " + nome);
+		System.out.println("Turno: " + turno);
+		System.out.println("Turma: " + turma);
+
+	}
+}
